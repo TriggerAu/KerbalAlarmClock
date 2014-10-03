bddc6659418b8ac0ad922d689697e63b2aa8bcd4
TriggerAu <dtregoning@outlook.com.au>
2014-10-03 17:36:18 +1000

    API Comments

--- KerbalAlarmClock_APITester/KACAPITester.cs
+++ KerbalAlarmClock_APITester/KACAPITester.cs
@@ -20,11 +20,13 @@ namespace KerbalAlarmClock_APITester
             LogFormatted("Start");
             KACWrapper.InitKACWrapper();
 
+            //Register the event handler
             KACWrapper.KAC.onAlarmStateChanged += KAC_onAlarmStateChanged;
         }
 
         void KAC_onAlarmStateChanged(KACWrapper.KACAPI.AlarmStateChangedEventArgs e)
         {
+            //output whats happened
             LogFormatted("{0}->{1}", e.alarm.Name, e.eventType);
         }
 
@@ -36,6 +38,7 @@ namespace KerbalAlarmClock_APITester
 
         internal override void OnDestroy()
         {
+            //destroy the event hook
             KACWrapper.KAC.onAlarmStateChanged -= KAC_onAlarmStateChanged;
         }
 
@@ -45,19 +48,23 @@ namespace KerbalAlarmClock_APITester
             GUILayout.Label("Instance: " + KACWrapper.InstanceExists.ToString());
             GUILayout.Label("APIReady: " + KACWrapper.APIReady.ToString());
 
+            //ifthe API hooked
             if (KACWrapper.APIReady)
             {
+                //Draw the alarms
                 foreach (KACWrapper.KACAPI.KACAlarm a in KACWrapper.KAC.Alarms)
                 {
                     GUILayout.BeginHorizontal();
                     GUILayout.Label(String.Format("{0}-{1}-{2} ({3})", a.Name, a.AlarmType,a.Notes,a.ID  ));
                     
+                    //Option to delete each alarm
                     if (GUILayout.Button("Delete",GUILayout.Width(50))) {
                             KACWrapper.KAC.DeleteAlarm(a.ID);
                         }
                     GUILayout.EndHorizontal();
                 }
 
+                //option to create a new alarm
                 if (GUILayout.Button("Create One"))
                 {
                     String aID = KACWrapper.KAC.CreateAlarm(KACWrapper.KACAPI.AlarmTypeEnum.TransferModelled, "Test",Planetarium.GetUniversalTime()+900);

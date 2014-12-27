b9968c02a0440f36e23781dedc7e354244ac61f4
TriggerAu <dtregoning@outlook.com.au>
2014-12-27 16:58:18 +1100

    Added extra script

---
+++ PluginBuilder/CommitVersionReady.ps1
@@ -0,0 +1,14 @@
+$PluginName = (get-item $PSScriptRoot).Parent.Name
+$UploadDir = "$($PSScriptRoot)\..\..\_Uploads\$($PluginName)"
+
+#Get newest version
+$Version =  (Get-ChildItem $UploadDir -Filter "v*.*.*.*"|sort -Descending)[0].name.replace("v","")
+
+$Choices= [System.Management.Automation.Host.ChoiceDescription[]] @("&Yes","&No")
+$ChoiceRtn = $host.ui.PromptForChoice("`r`nGit Commit - v$($Version)","Do you wish to Commit the version ready message?",$Choices,0)
+if ($ChoiceRtn -eq 0 )
+{
+    git checkout develop
+    git add -A * ..\*
+    git commit -m "Version $($Version) ready"
+}
